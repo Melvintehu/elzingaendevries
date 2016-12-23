d7bd61b298cd64ad6484ad7d9766f59bcfabc950
Melvin Tehubijuluw <melvin.tehubijuluw@beeproger.com>
2016-12-23 19:47:45 +0100

    Commitje

--- database/migrations/2016_12_23_181446_create_project_tag_table.php
+++ database/migrations/2016_12_23_181446_create_project_tag_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateTagsTable extends Migration
+class CreateProjectTagTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,12 @@ class CreateTagsTable extends Migration
      */
     public function up()
     {
-        Schema::create('tags', function (Blueprint $table) {
+        Schema::create('project_tag', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name')->nullable();
+
             $table->integer('project_id')->unsigned();
-            $table->foreign('project_id')->references('id')->on('projects');
-                        
+
             $table->timestamps();
-            $table->softDeletes();
         });
     }
 
@@ -31,9 +29,6 @@ class CreateTagsTable extends Migration
      */
     public function down()
     {
-        Schema::table('tags', function ($table) {
-            $table->dropForeign('tags_project_id_foreign');
-        });
-        Schema::dropIfExists('tags');
+        Schema::dropIfExists('project_tag');
     }
 }
